76769a525cc11ecf0dd0da276df514f7d68306ca
Rafael Aguiar <rafael.nascimento.aguiar@outlook.com>
2021-06-15 22:31:24 -0300

    add relation between users and appointments tables and alter provider field name to provider_id

--- backend/src/database/migrations/1623525230466-CreateAppointments.ts
+++ backend/src/database/migrations/1623525230466-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1623525230466 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
